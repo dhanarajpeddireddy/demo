a72bdc6f687c56c9c70f8e430c06edf6ddc1ff9a
dhanarajpeddireddy <34676522+dhanarajpeddireddy@users.noreply.github.com>
2019-05-05 15:54:00 +0530

    Update list.js

--- studentDemo/src/screens/list.js
+++ studentDemo/src/screens/list.js
@@ -1,64 +1,32 @@
 import React, { Component } from 'react'
 import { Text, View, TouchableOpacity, StyleSheet,Alert,ScrollView } from 'react-native'
-import * as firebase from "firebase";   
+
 class List extends Component {
     static navigationOptions = {
         title: 'Students',
       };
-
-      
-      constructor(props) {
-         super(props);
-       this.state = {
-            names: []
-            };
-         var firebase = require("firebase");
-         const config = {
-           apiKey: "AIzaSyA58VWic9JWxqBBi72ILJbVBrbtpuZ4Ivg",
-           authDomain: "studentdemo-123.firebaseapp.com",
-           databaseURL: "https://studentdemo-123.firebaseio.com",
-           projectId: "studentdemo-123",
-           storageBucket: "studentdemo-123.appspot.com",
-           messagingSenderId: "868386055442",
-           appId: "1:868386055442:web:67e5e366ca994fc9"
-         };
-         if (!firebase.apps.length) {
-           firebase.initializeApp(config);
-       }
-       }
-
-       componentDidMount()
-          {
-          
-           firebase.database().ref('students/') .once('value', function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-         
-              var childData = childSnapshot.val();
-                 alert("name :"+childData.name + " , group :"+childData.group + " , Roll num :"+childData.roll)
-
-            });
-          });
-          }
-       
-
+     
        
    alertItemName = (item) => {
-    Alert.alert(item.name)
-   }
+  alert("name :"+item.name + " , group :"+item.Group + " , Roll num :"+item.Roll +" , Address :"+item.Address+" , Mobile :"+item.mobile)
+         }
    render() {
+
+      const { navigation } = this.props;
+    let data = navigation.getParam('data', 'NO-ID');
+
       return (
          <View>
            <ScrollView>
             {
-               this.state.names.map((item, index) => (
+              data.map((item, index) => (
                   <TouchableOpacity
-                     key = {item.id}
                      style = {styles.container}
                      onPress = {() => this.alertItemName(item)}>
                      <Text style = {styles.text}>
                         {item.name}
                      </Text>
-                     <Text style = {styles.text}>
+                     <Text style = {styles.text2}>
                         {item.Roll}
                      </Text>
                   </TouchableOpacity>
@@ -80,5 +48,10 @@ const styles = StyleSheet.create ({
     padding: 10,
     fontSize: 18,
     height: 44,
-   }
+   },
+   text2: {
+      paddingLeft: 10,
+      fontSize: 18,
+      height: 42,
+     }
 })
