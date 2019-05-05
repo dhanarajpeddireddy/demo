0804aa444ec89e12ebe06f4a2e27b318b472bde7
dhanarajpeddireddy <34676522+dhanarajpeddireddy@users.noreply.github.com>
2019-05-05 15:53:04 +0530

    Update home.js

--- studentDemo/src/screens/home.js
+++ studentDemo/src/screens/home.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';  
 import { View, Text,StyleSheet,TouchableHighlight } from 'react-native';
+import * as firebase from "firebase";   
+var dataArray = [] ;
 export default class home extends Component {  
     static navigationOptions = {
         title: 'Home',
         
       };
+
+
+
+      constructor(props) {
+        super(props);
+      
+        var firebase = require("firebase");
+        const config = {
+          apiKey: "AIzaSyA58VWic9JWxqBBi72ILJbVBrbtpuZ4Ivg",
+          authDomain: "studentdemo-123.firebaseapp.com",
+          databaseURL: "https://studentdemo-123.firebaseio.com",
+          projectId: "studentdemo-123",
+          storageBucket: "studentdemo-123.appspot.com",
+          messagingSenderId: "868386055442",
+          appId: "1:868386055442:web:67e5e366ca994fc9"
+        };
+        if (!firebase.apps.length) {
+          firebase.initializeApp(config);
+      }
+      }
+
+      componentDidMount()
+         {
+          firebase.database().ref('students/') .on('value', function(snapshot) {
+           snapshot.forEach(function(childSnapshot) {
+             var childKey = childSnapshot.key;
+             var childData = childSnapshot.val();
+             dataArray.push( { name:childData.name ,Roll:childData.roll,Group:childData.group,Address:childData.address,mobile:childData.mobile});
+           });
+         });
+
+        
+         }
+
+
+
+
 
   render() {
     return (
@@ -15,7 +54,7 @@ export default class home extends Component {
             <Text style={styles.buttonText}>Add Student</Text>
           </View>
         </TouchableHighlight>
-        <TouchableHighlight   onPress={() => this.props.navigation.navigate('List')} underlayColor="white">
+        <TouchableHighlight   onPress={() => this.props.navigation.navigate('List',{data:dataArray})} underlayColor="white">
           <View style={styles.button}>
             <Text style={styles.buttonText}>View Students</Text>
           </View>
